f5335a21795f5c5521ea76fddb3b1a540006949f
shindaeyeong <eodud3196@gmail.com>
2024-03-25 15:48:28 +0900

    slot

---
+++ 06_slot/src/components/04_dynamicslot/DynamicParent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="parentContainer">
+        <h1>DynamicSlot: </h1>        
+        <input type="text" v-model="dynamicName">
+        {{  dynamicName }}
+    </div>
+</template>
+
+<script setup>
+    import {ref} from 'vue';
+
+    const dynamicName = ref('');
+</script>
+
+<style scoped>
+    .parentContainer {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        border: 1px solid black;
+    }
+
+</style>
